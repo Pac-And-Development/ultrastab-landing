c6ff591b0cf120f5a422655ba64b243555f17a56
Sorrowrest <nvas3470@gmail.com>
2021-12-28 19:17:04 +0300

    fix

---
+++ js/scroll.js
@@ -0,0 +1,141 @@
+const helper = {
+  getDelta(event) {
+    if (event.wheelDelta) {
+      return event.wheelDelta;
+    } else {
+      return -event.detail;
+    }
+  },
+  throttle(method, delay, context) {
+    let inThrottle = false;
+    return function () {
+      if (!inThrottle) {
+        inThrottle = true;
+        method.apply(context, arguments);
+        setTimeout(() => {
+          inThrottle = false;
+        }, delay);
+      }
+    };
+  },
+  debounce(method, delay, context) {
+    let inDebounce;
+    return function () {
+      clearTimeout(method.inDebounce);
+      inDebounce = setTimeout(() => {
+        method.apply(context, arguments);
+      }, delay);
+    };
+  },
+};
+class ScrollPages {
+  constructor(currentPageNumber, totalPageNumber, pages) {
+    this.currentPageNumber = currentPageNumber;
+    this.totalPageNumber = totalPageNumber;
+    this.pages = pages;
+    this.viewHeight = document.documentElement.clientHeight;
+    console.log(document.querySelector(".Head").clientHeight);
+
+    this.dopeHeights = document.querySelector(".Page").clientHeight;
+  }
+  mouseScroll(event) {
+    let delta = helper.getDelta(event);
+    if (delta < 0) {
+      this.scrollDown();
+    } else {
+      this.scrollUp();
+    }
+  }
+  scrollDown() {
+    if (this.currentPageNumber !== this.totalPageNumber) {
+      this.pages.style.top = -this.dopeHeights * this.currentPageNumber + "px";
+      this.currentPageNumber++;
+      console.log(this.currentPageNumber);
+      this.updateNav();
+    }
+  }
+  scrollUp() {
+    if (this.currentPageNumber !== 1) {
+      this.pages.style.top =
+        -this.dopeHeights * (this.currentPageNumber - 2) + "px";
+      this.currentPageNumber--;
+      console.log(this.currentPageNumber);
+      this.updateNav();
+    }
+  }
+  scrollTo(targetPageNumber) {
+    while (this.currentPageNumber !== targetPageNumber) {
+      if (this.currentPageNumber > targetPageNumber) {
+        this.scrollUp();
+      } else {
+        this.scrollDown();
+      }
+    }
+  }
+  createNav() {
+    const pageNav = document.createElement("div");
+    pageNav.className = "nav-dot-container";
+    this.pages.appendChild(pageNav);
+    for (let i = 0; i < this.totalPageNumber; i++) {
+      pageNav.innerHTML += '<p class="nav-dot"><span></span></p>';
+    }
+    const navDots = document.getElementsByClassName("nav-dot");
+    this.navDots = Array.prototype.slice.call(navDots);
+    this.navDots[0].classList.add("dot-active");
+    this.navDots.forEach((e, index) => {
+      e.addEventListener("click", (event) => {
+        this.scrollTo(index + 1);
+        this.navDots.forEach((e) => {
+          e.classList.remove("dot-active");
+        });
+        e.classList.add("dot-active");
+      });
+    });
+  }
+  updateNav() {
+    this.navDots.forEach((e) => {
+      e.classList.remove("dot-active");
+    });
+    this.navDots[this.currentPageNumber - 1].classList.add("dot-active");
+  }
+  resize() {
+    this.viewHeight = document.documentElement.clientHeight;
+    this.pages.style.height = this.viewHeight + "px";
+    this.pages.style.top =
+      -this.viewHeight * (this.currentPageNumber - 1) + "px";
+  }
+
+  init() {
+    let handleMouseWheel = helper.throttle(this.mouseScroll, 500, this);
+    let handleResize = helper.debounce(this.resize, 500, this);
+    this.pages.style.height = this.viewHeight + "px";
+    this.createNav();
+
+    if (navigator.userAgent.toLowerCase().indexOf("firefox") === -1) {
+      document.addEventListener("wheel", handleMouseWheel);
+    } else {
+      document.addEventListener("DOMMouseScroll", handleMouseWheel);
+    }
+    document.addEventListener("touchstart", (event) => {
+      this.startY = event.touches[0].pageY;
+    });
+    document.addEventListener("touchend", (event) => {
+      let endY = event.changedTouches[0].pageY;
+      if (this.startY - endY < 0) {
+        this.scrollUp();
+      }
+      if (this.startY - endY > 0) {
+        this.scrollDown();
+      }
+    });
+    document.addEventListener("touchmove", (event) => {
+      event.preventDefault();
+    });
+    window.addEventListener("resize", handleResize);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var s = new ScrollPages(1, 2, document.getElementById("all-pages"));
+  s.init();
+});
